49d9c9a468b611ca929be1f941c5ab8367bd9f13
YichiRockyZhang <rocky1oo.zhang@gmail.com>
2023-02-10 14:55:33 -0500

    adding to test index in ES

--- retriever.py
+++ retriever.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import List
+import json
 
 class Retriever(ABC):
 
@@ -30,13 +31,15 @@ class Retriever(ABC):
 class DPR_Retriever(Retriever):
     def __init__(self):
         # Metrics
-        self._metrics = ["sentence", "chunks", "paragraphs"]
+        self._metrics = ["sentence", "chunk", "paragraph"]
 
         # Set up elasticsearch
         es_user = "elastic"
         es_password = "RZJVsRjjv7ZeD6lezInf"
 
         from elasticsearch import Elasticsearch
+        from haystack.document_stores import ElasticsearchDocumentStore
+
         self.es = Elasticsearch(
             f"http://{es_user}:{es_password}@localhost:9200",
             use_ssl=True,
@@ -47,6 +50,39 @@ class DPR_Retriever(Retriever):
         # Ensure ES is running
         if (self.es is not None and self.es.ping() and
                 self.es.cluster.health()['status'] in ['yellow', 'green']):
+            doc_store = ElasticsearchDocumentStore(
+                host = "localhost",
+                port = 9200,
+                scheme = "https",
+                username = es_user,
+                password = es_password,
+                verify_certs = False,
+                index = "test"
+            )
+
+            with open("wiki_lookup.json", "r") as f:
+                wiki_json = json.loads(f.read())
+
+            data_json = []
+
+            for article in wiki_json:
+                pgs = article['text'].split('\n\n')
+                num_pgs = len(pgs)
+
+                for i, paragraph in enumerate(pgs):
+                    dict_to_add =\
+                        {
+                            'text': paragraph,
+                            'meta': {
+                                'source': article['title'],
+                                'id': article['id'],
+                                'paragraph_number': i,
+                                'paragraph_number_norm': i/num_pgs
+                            }
+                        }
+                    data_json.append(dict_to_add)
+            
+            doc_store.write_documents(data_json)
 
         else:
             raise("Elasticsearch cluster error")
@@ -56,7 +92,7 @@ class DPR_Retriever(Retriever):
             # Must include meta data for each document (Wiki page, sentence/chunks/paragraphs (normalized and unnormalized))
                 # Sentence = sent tokenizer
                 # Chunks = already written
-                # Paragraph = split by \n
+                # Paragraph = split by \n\n
             # Wikipedia is 500 MB --> Doing all 3 segmentations should be 1.5 GB total
 
     @property
